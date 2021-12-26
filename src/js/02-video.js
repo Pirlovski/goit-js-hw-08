2451489ba5573c98115988c5a3352fb320e861c8
Pirlovski <87051249+Pirlovski@users.noreply.github.com>
2021-12-26 14:03:23 +0300

    1
    1

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
 
+import throttle from 'lodash.throttle' ; 
+import Player from '@vimeo/player';
+
+
+const iframeEl = document.querySelector('#vimeo-player');
+
+const player = new Vimeo.Player(iframeEl);
+
+const onSaveTime = e => {
+  localStorage.setItem('videoplayer-current-time', e.seconds);
+  console.log(e.seconds);
+};
+const parsed = parseInt(localStorage.getItem('videoplayer-current-time'));
+console.log(parsed);
+
+const throttledSavedCurrentime = throttle(onSaveTime, 1000);
+player.on('timeupdate', throttledSavedCurrentime);
+
+player.setCurrentTime(parsed);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,48 @@
+// import throttle from 'lodash.throttle' ;
 
+const refs = {
+  form: document.querySelector('.feedback-form'),
+  input: document.querySelector('.feedback-form input') ,
+  textarea: document.querySelector('.feedback-form textarea') ,
+
+  
+};
+const FORM_KEY = "feedback-form-state";
+
+
+const onFormInput = evt => {
+  const messageEL = evt.currentTarget.elements;
+// console.dir(onFormInput) ;
+
+const mail = messageEL.email.value ; 
+const message = messageEL.message.value ;
+
+const dataToSaved = {
+
+  mail,
+  message,
+}
+
+localStorage.setItem('feedback-form-state' , JSON.stringify(dataToSaved)) ;
+};
+
+refs.form.addEventListener('input',onFormInput);
+
+
+const onPopulateForm = () => {
+  if (localStorage.getItem(FORM_KEY)) {
+      Object.entries(JSON.parse(localStorage.getItem(FORM_KEY))).forEach(([name, value]) => feedbackForm.elements[name].value = value); // `${name}: ${value}`; `${name}: value`; `${name} = value`
+  }
+};
+onPopulateForm();
+
+const onFormSubmit = event => {
+  event.preventDefault();
+  if (feedbackForm.elements.email.value && feedbackForm.elements.message.value !== "") {
+      console.log('Отправляем форму с данными: ', JSON.parse(localStorage.getItem(FORM_KEY)));
+      event.currentTarget.reset();
+      localStorage.removeItem(FORM_KEY);
+  };
+};
+
+feedbackForm.addEventListener("submit", onFormSubmit);
